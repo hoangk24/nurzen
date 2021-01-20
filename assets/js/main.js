8d4e7c10210f34117523bfabe7f6103dc2c19dad
hoangk24 <nguyentamhoang12a@gmail.com>
2021-01-20 13:53:57 +0700

    nguyentamhoang

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+
+
+
+var Click = {
+    Toggle : function (button,thing,excThing){
+        $(button).click(()=>{
+            $(thing).toggleClass(excThing);
+        })
+        
+    },
+    ActiveButton:(layout,button,excthing)=>{
+        const btns = document.querySelector(layout);
+        let listbtn = btns.querySelectorAll(button);
+  
+        listbtn.forEach((btn,index)=> {
+            $(btn).click(()=>{
+                listbtn.forEach( item => {
+                    $(item).removeClass(excthing);
+                })
+                $(listbtn[index]).addClass(excthing);
+             
+            })
+        });
+    }
+}
+var Scroll = {
+    ActiveElement:function(){
+        $(window).scroll(()=>{
+            if(window.scrollY > 0)  $('.header').addClass('header--active');
+            else $('.header').removeClass('header--active');
+         })
+    }
+}
+
+Scroll.ActiveElement();
+Click.ActiveButton('.gallery__content-button','.button-filter','button--active');
+Click.Toggle('.header__bars','.header__menu','menu--active');
+Click.Toggle('.header__search-button','.header__search-form','search--active');
+
+
+$grid = $('.gallery__content-filter').isotope({
+    itemSelector: '.filter-img',
+    layoutMode: 'fitRows',
+  });
+
+  $('.button-filter').on( 'click', function() {
+    var filterValue = $( this ).data('type');
+    var type = '.'+filterValue;
+    // use filterFn if matches value
+    $grid.isotope({ filter: type });
+  });
